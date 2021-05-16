694e671d5f5222f645478f11759ec024fa6b1fda
Clint <maxeva02@gmail.com>
2021-05-16 16:50:03 -0700

    moved folders from my last repo. Heroku wasn't loading

---
+++ user-directory2/src/components/SearchName/index.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import "./SearchName.css";
+import DataAreaContext from "../../utils/DataAreaContext";
+
+const SearchName = () => {
+    const context = useContext(DataAreaContext);
+
+    return (
+        <div className="searchbox">
+            <form className="form-inline">
+                <input
+                    className="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search for an employee"
+                    aria-label="Search"
+                    onChange={e => context.handleSearchChange(e)}
+                />
+                <button className="btn my-2 my-sm-0" type="submit">
+                    Who are you looking for?
+                 </button>
+            </form>
+        </div>
+    );
+}
+export default SearchName;
